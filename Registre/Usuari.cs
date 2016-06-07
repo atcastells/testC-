70d21bf1421eef78fb5509fbd768a2eb6a370f3b
acastells <aron.deltebre@live.com>
2016-06-07 13:07:22 +0200

    1st

---
+++ Registre/Usuari.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registre
+{
+    class Usuari
+    {
+        String nom;
+        String cognom;
+        String usuari;
+        String pass;
+
+        public Usuari(String nom, String cognom, String usuari, String pass)
+        {
+            this.nom = nom;
+            this.cognom = cognom;
+            this.usuari = usuari;
+            this.pass = pass;
+        }
+
+        /*Funcio PASS*/
+
+        /*Funció usuari*/
+    }
+}
